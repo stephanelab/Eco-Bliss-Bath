6d21df7ea8a3352f7ec13fb34fdd76d9273b891d
Stéphane LABROT <stephane.labrot@gmail.com>
2026-02-25 14:06:12 +0100

    test fonctionnalité login

--- frontend/cypress/e2e/login.cy.js
+++ frontend/cypress/e2e/login.cy.js
@@ -1,5 +1,6 @@
 describe("Login tests", () => {
   it("should login with correct credentials", () => {
+    cy.visit("/");
     cy.getBySel("nav-link-login").click();
     cy.fixture("user").then((user) => {
       cy.getBySel("login-input-username").type(user.username);
@@ -7,7 +8,7 @@ describe("Login tests", () => {
       cy.getBySel("login-submit").click();
     });
     cy.getBySel("nav-link-cart").should("be.visible");
-    cy.getBySel("nav-link-login").should("not.be.visible");
+    cy.getBySel("nav-link-login").should("not.exist");
     cy.getBySel("nav-link-logout").should("be.visible");
   });
 });
