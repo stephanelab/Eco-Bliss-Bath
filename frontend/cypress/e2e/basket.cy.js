71023ca2a7dd5c7e0ad9ea5ff47f21c44bc31d07
Stéphane LABROT <stephane.labrot@gmail.com>
2026-02-27 10:17:47 +0100

    Ajout d'un test sur panier + correction sur interception fiche produit (doit finir par un chiffre)

--- frontend/cypress/e2e/basket.cy.js
+++ frontend/cypress/e2e/basket.cy.js
@@ -7,29 +7,39 @@ describe("Basket tests", () => {
     if (retries <= 0) {
       throw new Error("Stock jamais positif après plusieurs tentatives");
     }
+    productId = -1;
+    cy.log(productId);
     cy.visit("/");
-    cy.intercept("GET", "**/products/*").as("getProduct");
+    cy.intercept("GET", "**/products/[0-9]*").as("getProduct");
     cy.getBySel("product-home-link").first().click();
-    cy.wait("@getProduct")
+    return cy
+      .wait("@getProduct")
       .its("response.body")
       .then((product) => {
+        cy.log("product = " + JSON.stringify(product));
         productId = product.id;
-      });
-    cy.getBySel("detail-product-stock")
-      .should(($el) => {
-        expect($el.text()).to.match(/\d/);
-      })
-      .invoke("text")
-      .then((text) => {
-        nstock = parseInt(text);
-        if (nstock <= 0) {
-          checkProductStock(retries - 1);
-        } else {
-          expect(nstock).to.be.greaterThan(0);
-        }
+        return cy
+          .getBySel("detail-product-stock")
+          .should(($el) => {
+            expect($el.text()).to.match(/\d/);
+          })
+          .invoke("text")
+          .then((text) => {
+            nstock = parseInt(text);
+            if (nstock <= 0) {
+              return checkProductStock(retries - 1);
+            }
+            expect(nstock).to.be.greaterThan(0);
+            cy.log(
+              "Selected product with id " + productId + " and stock " + nstock,
+            );
+            return cy.wrap(productId);
+          });
       });
   };
-  it("should have a good behaviour", () => {
+
+  beforeEach(() => {
+    // Connecter l'utilisateur
     cy.intercept("POST", "**/login").as("POSTlogin");
     cy.fixture("user").then((user) =>
       cy.loginFront(user.username, user.password),
@@ -40,13 +50,17 @@ describe("Basket tests", () => {
       .then((token) => {
         cy.deleteBasket(token);
       });
-    //
-    // Lancer la fonction pour sélectionner un produit et vérifier que le stock du produit est positif
-    checkProductStock(3);
-    // Récupérer l'URL du produit sélectionné pour pouvoir y revenir plus tard
-    cy.url().then((url) => {
-      cy.wrap(url).as("savedUrl");
-    });
+  });
+
+  it("should not add an unavailable product in the basket", () => {
+    // Naviguer sur une fiche produit dont le stock est <0
+    cy.intercept("GET", "**/products/3").as("getProduct");
+    cy.visit("/products/3");
+    cy.wait("@getProduct")
+      .its("response.body")
+      .then((product) => {
+        expect(product.availableStock).to.be.lessThan(1);
+      });
     // Cliquer sur le bouton "Ajouter au panier"
     cy.intercept("GET", "**/orders").as("getBasket");
     cy.getBySel("detail-product-add").click();
@@ -55,44 +69,69 @@ describe("Basket tests", () => {
       .its("response.body.orderLines")
       .then((orderLines) => {
         const addedProduct = orderLines.find(
-          (line) => line.product.id === productId && line.quantity === 1,
+          (OrderLine) => OrderLine.product.id === 3,
         );
-        expect(addedProduct).to.exist;
+        expect(addedProduct).to.not.exist;
       });
-    // Vérifier que l'on soit redirigé vers la page du panier
-    cy.url().should("include", "/cart");
-    // Vérifier que le produit ait été ajouté au panier
-    cy.getBySel("cart-line").should("have.length", 1);
-    // Retour sur la page du produit en utilisant l'URL sauvegardé
-    cy.get("@savedUrl").then((savedUrl) => {
-      cy.visit(savedUrl);
-    });
-    // Vérifier que le stock du produit ait été décrémenté de 1
-    cy.getBySel("detail-product-stock")
-      .should("exist") // Vérifier que le champ de disponibilité soit présent
-      .should(($el) => {
-        expect($el.text()).to.match(/\d/);
-      })
-      .invoke("text")
-      .then((text) => {
-        const nstocka = parseInt(text);
-        expect(nstocka).to.eq(nstock - 1);
+  });
+
+  it("should have a good behaviour", () => {
+    // Lancer la fonction pour sélectionner un produit et vérifier que le stock du produit est positif
+    checkProductStock().then((productId) => {
+      // Récupérer l'URL du produit sélectionné pour pouvoir y revenir plus tard
+      cy.url().then((url) => {
+        cy.wrap(url).as("savedUrl");
       });
-    // Taper un chiffre négatif dans le champ de quantité du produit
-    cy.getBySel("detail-product-quantity").clear().type("-1");
-    // Vérifier qu'on ne puisse pas valider la modification de la quantité en vérifiant qu'on ne soit pas rediriger vers le panier
-    cy.getBySel("detail-product-add").click();
-    cy.wait(500); // Attendre un peu pour s'assurer que la redirection aurait eu le temps de se faire si elle devait se faire
-    cy.get("@savedUrl").then((savedUrl) => {
-      cy.url().should("eq", savedUrl);
-    });
-    // Taper un chiffre supérieur à 20 dans le champ de quantité du produit
-    cy.getBySel("detail-product-quantity").clear().type("50");
-    // Vérifier qu'on ne puisse pas valider la modification de la quantité en vérifiant qu'on ne soit pas rediriger vers le panier
-    cy.getBySel("detail-product-add").click();
-    cy.wait(500); // Attendre un peu pour s'assurer que la redirection aurait eu le temps de se faire si elle devait se faire
-    cy.get("@savedUrl").then((savedUrl) => {
-      cy.url().should("eq", savedUrl);
+      // Cliquer sur le bouton "Ajouter au panier"
+      cy.intercept("GET", "**/orders").as("getBasket");
+      cy.getBySel("detail-product-add").click();
+      cy.wait("@getBasket")
+        // Vérifier que le produit ait été ajouté au panier en vérifiant que la réponse de l'API contienne une ligne de commande avec l'id du produit sélectionné etune quantité de 1
+        .its("response.body.orderLines")
+        .then((orderLines) => {
+          cy.log("productId = " + productId);
+          const addedProduct = orderLines.find(
+            (OrderLine) =>
+              OrderLine.product.id === Number(productId) &&
+              OrderLine.quantity === 1,
+          );
+          expect(addedProduct).to.exist;
+        });
+      // Vérifier que l'on soit redirigé vers la page du panier
+      cy.url().should("include", "/cart");
+      // Vérifier que le produit ait été ajouté au panier
+      cy.getBySel("cart-line").should("have.length", 1);
+      // Retour sur la page du produit en utilisant l'URL sauvegardé
+      cy.get("@savedUrl").then((savedUrl) => {
+        cy.visit(savedUrl);
+      });
+      // Vérifier que le stock du produit ait été décrémenté de 1
+      cy.getBySel("detail-product-stock")
+        .should("exist") // Vérifier que le champ de disponibilité soit présent
+        .should(($el) => {
+          expect($el.text()).to.match(/\d/);
+        })
+        .invoke("text")
+        .then((text) => {
+          const nstocka = parseInt(text);
+          expect(nstocka).to.eq(nstock - 1);
+        });
+      // Taper un chiffre négatif dans le champ de quantité du produit
+      cy.getBySel("detail-product-quantity").clear().type("-1");
+      // Vérifier qu'on ne puisse pas valider la modification de la quantité en vérifiant qu'on ne soit pas rediriger vers le panier
+      cy.getBySel("detail-product-add").click();
+      cy.wait(500); // Attendre un peu pour s'assurer que la redirection aurait eu le temps de se faire si elle devait se faire
+      cy.get("@savedUrl").then((savedUrl) => {
+        cy.url().should("eq", savedUrl);
+      });
+      // Taper un chiffre supérieur à 20 dans le champ de quantité du produit
+      cy.getBySel("detail-product-quantity").clear().type("50");
+      // Vérifier qu'on ne puisse pas valider la modification de la quantité en vérifiant qu'on ne soit pas rediriger vers le panier
+      cy.getBySel("detail-product-add").click();
+      cy.wait(500); // Attendre un peu pour s'assurer que la redirection aurait eu le temps de se faire si elle devait se faire
+      cy.get("@savedUrl").then((savedUrl) => {
+        cy.url().should("eq", savedUrl);
+      });
     });
   });
 });
